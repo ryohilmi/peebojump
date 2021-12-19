bfc93638a2354711816ab563a34bfa177b70d070
zahrafayya <zahra.fayyadiyati@gmail.com>
2021-12-19 22:41:20 +0700

    feat: add stretchSpeed

--- src/Entity/MapObject.java
+++ src/Entity/MapObject.java
@@ -7,30 +7,31 @@ import TileMap.Tile;
 import java.awt.Rectangle;
 
 public abstract class MapObject {
-	
+
+	protected boolean hitboxFlag = false; // TODO: remove pas production
 	// tile stuff
 	protected TileMap tileMap;
 	protected int tileSize;
 	protected double xmap;
 	protected double ymap;
-	
+
 	// position and vector
 	protected double x;
 	protected double y;
 	protected double dx;
 	protected double dy;
-	
+
 	// dimensions
 	protected static final int CONSTWIDTH = 32;
 	protected int width;
 	protected int height;
-	
+
 	// collision box
 	protected int cwidth;
 	protected int cheight;
 	protected int mincwidth;
 	protected int maxcwidth;
-	
+
 	// collision
 	protected int currRow;
 	protected int currCol;
@@ -46,13 +47,14 @@ public abstract class MapObject {
 
 	// stretching collision
 	protected boolean stretchDone;
-	
+	protected int stretchSpeed;
+
 	// animation
 	protected Animation animation;
 	protected int currentAction;
 	protected int previousAction;
 	protected boolean facingRight;
-	
+
 	// movement
 	protected boolean left;
 	protected boolean right;
@@ -60,7 +62,7 @@ public abstract class MapObject {
 	protected boolean down;
 	protected boolean jumping;
 	protected boolean falling;
-	
+
 	// movement attributes
 	protected double moveSpeed;
 	protected double maxSpeed;
@@ -73,16 +75,16 @@ public abstract class MapObject {
 	// constructor
 	public MapObject(TileMap tm) {
 		tileMap = tm;
-		tileSize = tm.getTileSize(); 
+		tileSize = tm.getTileSize();
 	}
-	
+
 	public boolean intersects(MapObject o) {
 		Rectangle r1 = getRectangle();
 		Rectangle r2 = o.getRectangle();
 		//System.out.println(r1.intersects(r2));
 		return r1.intersects(r2);
 	}
-	
+
 	public Rectangle getRectangle() {
 		if(facingRight && right) {
 			return new Rectangle((int)(xtemp + xmap), (int)(ytemp + ymap), cwidth, cheight);
@@ -94,37 +96,37 @@ public abstract class MapObject {
 
 		return new Rectangle((int)(xtemp + xmap), (int)(ytemp + ymap), cwidth, cheight);
 	}
-	
+
 	public void calculateCorners(double x, double y) {
-		
+
 		int leftTile = (int)(x - cwidth / 2) / tileSize;
 		int rightTile = (int)(x + cwidth / 2 - 1) / tileSize;
 		int topTile = (int)(y - cheight / 2) / tileSize;
 		int bottomTile = (int)(y + cheight / 2 - 1) / tileSize;
-		
+
 		int tl = tileMap.getType(topTile, leftTile);
 		int tr = tileMap.getType(topTile, rightTile);
 		int bl = tileMap.getType(bottomTile, leftTile);
 		int br = tileMap.getType(bottomTile, rightTile);
-		
+
 		topLeft = tl == Tile.BLOCKED;
 		topRight = tr == Tile.BLOCKED;
 		bottomLeft = bl == Tile.BLOCKED;
 		bottomRight = br == Tile.BLOCKED;
-		
+
 	}
-	
+
 	public void checkTileMapCollision() {
-		
+
 		currCol = (int)x / tileSize;
 		currRow = (int)y / tileSize;
-		
+
 		xdest = x + dx;
 		ydest = y + dy;
-		
+
 		xtemp = x;
 		ytemp = y;
-		
+
 		calculateCorners(x, ydest);
 		if(dy < 0) {
 			if(topLeft || topRight) {
@@ -145,7 +147,7 @@ public abstract class MapObject {
 				ytemp += dy;
 			}
 		}
-		
+
 		calculateCorners(xdest, y);
 		if(dx < 0) {
 			if(topLeft || bottomLeft) {
@@ -165,44 +167,44 @@ public abstract class MapObject {
 				xtemp += dx;
 			}
 		}
-		
+
 		if(!falling) {
 			calculateCorners(x, ydest + 1);
 			if(!bottomLeft && !bottomRight) {
 				falling = true;
 			}
 		}
-		
+
 	}
 
 	public void stretchCollision () {
 		if(facingRight && right) {
-			if (cwidth < maxcwidth) cwidth++;
-			else if (cwidth == maxcwidth) {
+			if (cwidth < maxcwidth) cwidth += stretchSpeed;
+			else if (cwidth >= maxcwidth) {
 				cwidth = mincwidth;
 				stretchDone = true;
 			}
 		}
 		else if(!facingRight && left) {
 			if (cwidth < maxcwidth) {
-				cwidth++;
-				xinverse--;
+				cwidth += stretchSpeed;
+				xinverse -= stretchSpeed;
 			}
-			else if (cwidth == maxcwidth) {
+			else if (cwidth >= maxcwidth) {
 				cwidth = mincwidth;
 				xinverse = 0;
 				stretchDone = true;
 			}
 		}
 	}
-	
+
 	public int getx() { return (int)x; }
 	public int gety() { return (int)y; }
 	public int getWidth() { return width; }
 	public int getHeight() { return height; }
 	public int getCWidth() { return cwidth; }
 	public int getCHeight() { return cheight; }
-	
+
 	public void setPosition(double x, double y) {
 		this.x = x;
 		this.y = y;
@@ -211,49 +213,49 @@ public abstract class MapObject {
 		this.dx = dx;
 		this.dy = dy;
 	}
-	
+
 	public void setMapPosition() {
 		xmap = tileMap.getx();
 		ymap = tileMap.gety();
 	}
-	
+
 	public void setLeft(boolean b) { left = b; }
 	public void setRight(boolean b) { right = b; }
 	public void setUp(boolean b) { up = b; }
 	public void setDown(boolean b) { down = b; }
 	public void setJumping(boolean b) { jumping = b; }
-	
+
 	public boolean notOnScreen() {
 		return x + xmap + width < 0 ||
-			x + xmap - width > GamePanel.WIDTH ||
-			y + ymap + height < 0 ||
-			y + ymap - height > GamePanel.HEIGHT;
+				x + xmap - width > GamePanel.WIDTH ||
+				y + ymap + height < 0 ||
+				y + ymap - height > GamePanel.HEIGHT;
 	}
-	
+
 	public void draw(java.awt.Graphics2D g) {
 		if(facingRight) {
 			g.drawImage(
-				animation.getImage(),
-				(int)(x + xmap - width / 2),
-				(int)(y + ymap - height / 2),
-				null
+					animation.getImage(),
+					(int)(x + xmap - width / 2),
+					(int)(y + ymap - height / 2),
+					null
 			);
 		}
 		else {
 			g.drawImage(
-				animation.getImage(),
-				(int)(x + xmap - width / 2 + width),
-				(int)(y + ymap - height / 2),
-				-width,
-				height,
-				null
+					animation.getImage(),
+					(int)(x + xmap - width / 2 + width),
+					(int)(y + ymap - height / 2),
+					-width,
+					height,
+					null
 			);
 		}
-		if(facingRight && right) { g.drawRect((int)(xtemp + xmap), (int)(ytemp + ymap), cwidth, cheight); }
-		if(!facingRight && left) { g.drawRect((int)(xinverse + xtemp + xmap), (int)(ytemp + ymap), cwidth, cheight); }
+		if(facingRight) { g.drawRect((int)(xtemp + xmap), (int)(ytemp + ymap), cwidth, cheight); }
+		if(!facingRight) { g.drawRect((int)(xinverse + xtemp + xmap), (int)(ytemp + ymap), cwidth, cheight); }
 
 	}
-	
+
 }
 
 
