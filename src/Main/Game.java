d3d6b758f57a44f026cae6464b9207083865449b
Marcellino Mahesa <marcellino.janitra@yahoo.com>
2021-12-21 01:24:25 +0700

    fix: add explosion on level 3, add gitignore and msg on main

--- src/Main/Game.java
+++ src/Main/Game.java
@@ -7,6 +7,9 @@ import javax.swing.*;
 public class Game {
 
     public static void main(String[] args) {
+        // Audio terkadang akan fail buat di load
+        // possible cause: karena versi sdk / setting
+        // message: unsupported format
         JFrame window = new JFrame("PeeboJump");
         window.setContentPane(new GamePanel());
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
